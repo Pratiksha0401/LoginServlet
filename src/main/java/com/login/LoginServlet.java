d1723cbc6e064434055dc91b3d85f7fbb81e8fc2
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-06-15 19:32:01 +0530

    Added : code to create simple login with predefined user

--- src/main/java/com/login/LoginServlet.java
+++ src/main/java/com/login/LoginServlet.java
@@ -3,26 +3,44 @@ package com.login;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LoginServlet
- */
-@WebServlet("/LoginServlet")
-public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		out.println("<h3>Hello World Pratiksha</h3>");
-		out.close();
-	}
 
-}
+@WebServlet(
+	     description = "Login servlet Testing",
+	     urlPatterns = {"/LoginServlet"},
+	     initParams = {
+			     @WebInitParam(name="user", value="Pratiksha"),
+			     @WebInitParam(name ="password", value="Bridge")
+	      }
+	)
+
+	public class LoginServlet extends HttpServlet {
+		private static final long serialVersionUID = 1L;
+	       
+		@Override
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			String user = request.getParameter("user");
+			String pwd = request.getParameter("pwd");
+			
+			String userId = getServletConfig().getInitParameter("user");
+			String password = getServletConfig().getInitParameter("password");
+			
+			if(userId.equals(user)&& password.equals(pwd)) {
+				request.setAttribute("user", user);
+				request.getRequestDispatcher("Loginsuccess.jsp").forward(request, response);
+				
+			}else {
+				RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+				PrintWriter out = response.getWriter();
+				out.println("<font color = red>Either user name or password is wrong</font>");
+				rd.include(request, response);
+			}
+		}
+	}
